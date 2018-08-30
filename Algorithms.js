12257d671e31310f3c239aff0213848e65821a1d
Peter Estes <peternigelestes@gmail.com>
2018-08-30 15:10:09 -0400

    reverse and palindrome

--- Algorithms.js
+++ Algorithms.js
@@ -1,19 +1,33 @@
 // FIZZBUZZ
 
-function fizzbuzz(n) {
-  const arr = []
-  for (let i = 1; i < n; i++) {
-    const val =
-      i % 15 === 0
-        ? "fizzbuzz"
-        : i % 5 === 0
-          ? "fizz"
-          : i % 3 === 0
-            ? "buzz"
-            : i
+// function fizzbuzz(n) {
+//   const arr = []
+//   for (let i = 1; i < n; i++) {
+//     const val =
+//       i % 15 === 0
+//         ? "fizzbuzz"
+//         : i % 5 === 0
+//           ? "fizz"
+//           : i % 3 === 0
+//             ? "buzz"
+//             : i
 
-    arr.push(val)
-  }
-  return arr
-}
-fizzbuzz(100)
+//     arr.push(val)
+//   }
+//   return arr
+// }
+// fizzbuzz(100)
+
+// REVERSE
+
+// function reverse(str) {
+//     return str.split("").reduce((acc, char) => char + acc, "")
+// }
+// reverse ("thingy")
+
+// PALINDROME
+
+const palindrome = word =>
+  word === word.split("").reduce((acc, char) => char + acc, "")
+
+palindrome("racecar")
